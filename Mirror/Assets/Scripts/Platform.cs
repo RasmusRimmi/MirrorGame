1c19ee8c04d9461ca225c20941eee662710f86f8
RasmusRimmi <rasmus136@gmail.com>
2023-03-31 13:59:59 +0300

    Moving platforms and traps

--- Mirror/Assets/Scripts/Platform.cs
+++ Mirror/Assets/Scripts/Platform.cs
@@ -10,12 +10,22 @@ public class Platform : MonoBehaviour
         {
             collision.gameObject.transform.SetParent(transform);
         }
+
+        if (collision.gameObject.name == "Player2")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
     }
 
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player1")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+
+        if (collision.gameObject.name == "Player2")
         {
             collision.gameObject.transform.SetParent(null);
         }
